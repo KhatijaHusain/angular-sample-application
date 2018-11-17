f369cb9342adcfb6b863a1c9c90f97ade63eaaa0
Khatija Husain <khatija.husain7@gmail.com>
2018-11-17 12:30:12 +0200

    Filtering of products, use of life cycle hook OnInit
    Filtering of products based on the value entered in the textbox

--- my-new-app/src/app/products/product-list.component.ts
+++ my-new-app/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -7,34 +7,59 @@ import { IProduct } from './product';
     styleUrls: ['./product-list.component.less']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 title = 'Product List';
 showImage = true;
-listFilter = 'cart';
+_listfilter: string;
 
-products : IProduct[] = [
+get listFilter(): string {
+    return this._listfilter;
+
+}
+set listFilter(value: string) {
+    this._listfilter = value;
+    this.filteredProducts = this._listfilter ? this.performFilter(this.listFilter) : this.products;
+}
+
+
+filteredProducts: IProduct[];
+products: IProduct[] = [
     {
         'productId': 1,
         'productName': 'Celini Latest Collection',
-        'productCode': 'a100',
+        'productCode': 'a-100',
         'productPrice': 2000,
         'productImage' : './assets/Images/purse1.jpg'
     },
     {
         'productId': 2,
         'productName': 'Celini Latest Collection',
-        'productCode': 'a200',
+        'productCode': 'a-200',
         'productPrice': 2000,
         'productImage' : './assets/Images/purse2.jpg'
     },
     {
-        'productId': 1,
+        'productId': 3,
         'productName': 'Handmade Exclusive',
-        'productCode': 'a300',
+        'productCode': 'a-300',
         'productPrice': 3000,
         'productImage' : './assets/Images/purse3.jpg'
     }
 ];
+
+constructor() {
+    this.filteredProducts = this.products;
+}
+ngOnInit(): void {
+ console.log('In Onit');
+}
+
+
+performFilter(filterValue: string): IProduct[] {
+    filterValue = filterValue.toLocaleLowerCase();
+return this.products.filter((product: IProduct) =>
+product.productName.toLocaleLowerCase().indexOf(filterValue) !== -1);
+}
 
 toggleImage(): void {
 this.showImage = !this.showImage;
