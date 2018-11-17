593bee081c8c48a74cd1e67a55f99dfa84192b21
Khatija Husain <khatija.husain7@gmail.com>
2018-11-17 09:54:02 +0200

    *ngIf,  *ngFor, event binding and event handling
    How to use  *ngIf, *ngFor, property binding, ,event binding and handling.

--- my-new-app/src/app/products/product-list.component.ts
+++ my-new-app/src/app/products/product-list.component.ts
@@ -7,4 +7,33 @@ import {Component} from '@angular/core';
 
 export class ProductListComponent {
 title = 'Product List';
+showImage = true;
+
+products = [
+    {
+        'productId': 1,
+        'productName': 'Celini Latest Collection',
+        'productCode': 'a100',
+        'productPrice': 2000,
+        'productImage' : './assets/Images/purse1.jpg'
+    },
+    {
+        'productId': 2,
+        'productName': 'Celini Latest Collection',
+        'productCode': 'a200',
+        'productPrice': 2000,
+        'productImage' : './assets/Images/purse2.jpg'
+    },
+    {
+        'productId': 1,
+        'productName': 'Handmade Exclusive',
+        'productCode': 'a300',
+        'productPrice': 3000,
+        'productImage' : './assets/Images/purse3.jpg'
+    }
+];
+
+toggleImage(): void {
+this.showImage = !this.showImage;
+}
 }
