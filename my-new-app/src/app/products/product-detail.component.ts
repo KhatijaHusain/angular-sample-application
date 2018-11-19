e1da38a7569acc54c9e63c73ecb3b807b7ce144c
Khatija Husain <khatija.husain7@gmail.com>
2018-11-19 19:35:45 +0200

    Routing and navigation
    Using activated route to redirect to the details page

--- my-new-app/src/app/products/product-detail.component.ts
+++ my-new-app/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+pageTitle = 'Product Detail';
+product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+                'productId': id,
+                'productName': 'Foschini Exclusive',
+                'productCode': 'a-600',
+                'productPrice': 3500,
+                'productImage' : './assets/Images/purse6.jpg',
+                'productRating': 3
+
+    }
   }
 
 }
